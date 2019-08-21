a89fc49c030c5303c7c90a18e2fe9d0944997240
kentonp <kentonp@drumeo.com>
2019-08-21 10:25:43 -0700

    New API functions

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 import axios from 'axios';
 import Railcontent from './src/railcontent';
+import Daily_statistics from './src/daily-statistics';
+import Access_codes from './src/access-codes';
+import Cart from './src/cart';
+import Order from './src/order';
+import Shipping from './src/shipping';
+import Payment_method from './src/payment-method';
+import Payments from './src/payments';
+import Products from './src/products';
 
 /**
  * Configure the instance of axios with some defaults
@@ -25,13 +33,37 @@ export function configure({
 
 // Export every function individually
 export * from './src/railcontent';
+export * from './src/daily-statistics';
+export * from './src/access-codes';
+export * from './src/cart';
+export * from './src/order';
+export * from './src/shipping';
+export * from './src/payment-method';
+export * from './src/payments';
+export * from './src/products';
 
 // Export every module individually
 export {
     Railcontent,
+    Daily_statistics,
+    Access_codes,
+    Cart,
+    Order,
+    Shipping,
+    Payment_method,
+    Payments,
+    Products,
 };
 
 // Create Services object to export as default
 export default {
     Railcontent,
+    Daily_statistics,
+    Access_codes,
+    Cart,
+    Order,
+    Shipping,
+    Payment_method,
+    Payments,
+    Products, 
 };

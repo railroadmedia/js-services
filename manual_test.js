d9447e1d8901383baf52c044ec36aef4d9943ac0
Curtis <curtis@drumeo.com>
2019-11-30 13:13:40 -0800

    Did this make the build fail?

--- manual_test.js
+++ manual_test.js
@@ -1,17 +1,17 @@
-import { configure, getContent } from './index';
-
-(async function init() {
-    configure({
-        baseURL: 'https://staging.musora.com',
-    });
-
-    const { response, error } = await getContent({
-        included_types: ['song', 'play-along'],
-    });
-
-    if (error) {
-        return console.log(error);
-    }
-
-    return console.log(response);
-}());
+// import { configure, getContent } from './index';
+//
+// (async function init() {
+//     configure({
+//         baseURL: 'https://staging.musora.com',
+//     });
+//
+//     const { response, error } = await getContent({
+//         included_types: ['song', 'play-along'],
+//     });
+//
+//     if (error) {
+//         return console.log(error);
+//     }
+//
+//     return console.log(response);
+// }());
